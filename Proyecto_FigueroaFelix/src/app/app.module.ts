22b16da52d0c451d9557d54405f166ed2134afcf
felix9941 <felixfigueroa41@gmail.com>
2022-08-08 12:20:46 -0300

    parte de experiencia y trabajo part1

--- Proyecto_FigueroaFelix/src/app/app.module.ts
+++ Proyecto_FigueroaFelix/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LogoApComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     LogoApComponent,
     SocialComponent,
     BannerComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule,
